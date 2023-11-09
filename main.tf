53791ed112f45134c36718896b8d032f34e8fe3a
mayank <mayankpandey20@gmail.com>
2023-11-09 23:55:13 +0000

    task def os runtime updated

--- main.tf
+++ main.tf
@@ -52,6 +52,10 @@ resource "aws_ecs_task_definition" "my_first_task" {
   memory                   = 512         # Specifying the memory our container requires
   cpu                      = 256         # Specifying the CPU our container requires
   execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+  runtime_platform {
+    cpu_architecture = "X86_64"
+    operating_system_family = "LINUX"
+  }
 }
 
 resource "aws_iam_role" "ecsTaskExecutionRole" {
